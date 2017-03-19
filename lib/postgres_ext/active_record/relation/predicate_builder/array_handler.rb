e55e3a95d3a9529e1798f860fe856c449f6b6362
Hamed Asghari <hasghari@g2crowd.com>
2017-03-19 16:35:19 -0700

    Fix deprecation warnings

--- lib/postgres_ext/active_record/relation/predicate_builder/array_handler.rb
+++ lib/postgres_ext/active_record/relation/predicate_builder/array_handler.rb
@@ -6,26 +6,21 @@ require 'active_support/concern'
 module ActiveRecord
   class PredicateBuilder
     module ArrayHandlerPatch
-      extend ActiveSupport::Concern
-
-      included do
-        def call_with_feature(attribute, value)
-          column = case attribute.try(:relation)
-                   when Arel::Nodes::TableAlias, NilClass
-                   else
-                     cache = ActiveRecord::Base.connection.schema_cache
-                     if cache.table_exists? attribute.relation.name
-                       cache.columns(attribute.relation.name).detect{ |col| col.name.to_s == attribute.name.to_s }
-                     end
+      def call(attribute, value)
+        column = case attribute.try(:relation)
+                 when Arel::Nodes::TableAlias, NilClass
+                 else
+                   cache = ActiveRecord::Base.connection.schema_cache
+                   exists_method_name = ::ActiveRecord::VERSION::MAJOR >= 5 ? :data_source_exists? : :table_exists?
+                   if cache.public_send(exists_method_name, attribute.relation.name)
+                     cache.columns(attribute.relation.name).detect{ |col| col.name.to_s == attribute.name.to_s }
                    end
-          if column && column.respond_to?(:array) && column.array
-            attribute.eq(value)
-          else
-            call_without_feature(attribute, value)
-          end
+                 end
+        if column && column.respond_to?(:array) && column.array
+          attribute.eq(value)
+        else
+          super(attribute, value)
         end
-
-        alias_method_chain(:call, :feature)
       end
 
       module ClassMethods
@@ -35,4 +30,4 @@ module ActiveRecord
   end
 end
 
-ActiveRecord::PredicateBuilder::ArrayHandler.send(:include, ActiveRecord::PredicateBuilder::ArrayHandlerPatch)
+ActiveRecord::PredicateBuilder::ArrayHandler.prepend ActiveRecord::PredicateBuilder::ArrayHandlerPatch
